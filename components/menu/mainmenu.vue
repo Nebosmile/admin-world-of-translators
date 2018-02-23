22fc096ec0cfdb4deb7ce418350cd866f0ad8a29
Pavel <akulov_pavel88@mail.ru>
2018-02-23 14:42:24 +0200

    up menu to layots

---
+++ components/menu/mainmenu.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  	<div class="menu_wrap container">
+  		<ul class="menu">
+  			<li v-for='(elem, index) in menu'>
+				<router-link :to='index'>{{elem}}</router-link>
+			</li>
+  		</ul>
+  	</div>
+</template>
+
+<script>
+export default {
+	name:'mainmenu',
+	data(){
+		return{
+			menu:{
+				dictionary:"dictionaryText",
+				dictionary2:"dictionaryText2",
+			}
+		}
+	}
+}
+</script>
+
+<style lang="css">
+</style>
--- layouts/base_page.vue
+++ layouts/base_page.vue
@@ -1,7 +1,64 @@
 <template>
-    <div class="">
+    <div class="main_wrap">
+        <div class="menu_section">
+            <mainmenu></mainmenu>
+        </div>
+
         <h1>Добро пожаловать на главню страницу</h1>
         <nuxt/>
     </div>
 
 </template>
+
+<script>
+
+import mainmenu from '@/components/menu/mainmenu'
+export default {
+    components: {
+       mainmenu
+    }
+};
+</script>
+<style lang='scss'>
+body{
+    margin: 0;
+    padding:0;
+}
+ul{
+    padding: 0;
+    margin: 0;
+}
+.menu_section{
+    border-bottom: solid 1px #8a8a8a;
+
+    .menu_wrap{
+        .menu{
+            display: flex;
+            li{
+                list-style: none;
+                padding: 5px;
+                a{
+                    width: 100%;
+                    display: inline-block;
+                    color: #000;
+                    text-transform: uppercase;
+                    text-decoration: none;
+                    border-left: solid 1px #000;
+                    padding: 7px;
+                }
+                a:hover{
+                    text-decoration: underline;
+                }
+
+            }
+            li:first-child a{
+                border-left: none;
+            }
+        }
+    }
+}
+
+section{
+    width: 100%;
+}
+</style>
